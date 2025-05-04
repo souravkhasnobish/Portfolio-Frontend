734ee46d3681c5a08f23a42ae29287b0376385ef
Sourav Khasnobish <souravkhasnobish018@gmail.com>
2025-05-04 20:21:43 +0530

    code changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,54 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:5000/api/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        Swal.fire({
+          title: "Thank You! 💛",
+          icon: "success",
+          confirmButtonText: "I will be connecting with you shortly!",
+          confirmButtonColor: "#d2691e",
+        });
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
+      } else {
+        alert(`Error: ${data.error}`);
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Server error!");
+    }
+  };
+
   return (
     <>
       <div
@@ -8,7 +58,10 @@ const Contact = () => {
         <h1 className="text-3xl font-bold mb-4">Contact me</h1>
         <span>Please fill out the form below to contact me</span>
         <div className="flex flex-col items-center justify-center">
-          <form className="bg-slate-200 w-full max-w-md px-4 py-6 rounded-xl mt-8">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-slate-200 w-full max-w-md px-4 py-6 rounded-xl mt-8"
+          >
             <h1 className="text-xl font-semibold mb-4 text-black">
               Send Your Message
             </h1>
@@ -19,6 +72,8 @@ const Contact = () => {
                 type="text"
                 id="name"
                 name="name"
+                value={formData.name}
+                onChange={handleChange}
                 placeholder="Enter your fullname"
                 required
               />
@@ -31,6 +86,8 @@ const Contact = () => {
                 type="email"
                 id="email"
                 name="email"
+                value={formData.email}
+                onChange={handleChange}
                 placeholder="Enter your email address"
                 required
               />
@@ -42,6 +99,8 @@ const Contact = () => {
                 className="shadow rounded-lg py-2 px-3 leading-light bg-white text-black w-full"
                 id="message"
                 name="message"
+                value={formData.message}
+                onChange={handleChange}
                 placeholder="Enter your Query"
                 required
               />
